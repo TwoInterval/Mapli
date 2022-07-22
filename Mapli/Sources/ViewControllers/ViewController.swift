a611069f61576d7389cbe50ede0df1a512fecd89
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-07-22 20:04:17 +0900

    Product Setting

---
+++ Mapli/Sources/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mapli
+//
+//  Created by woo0 on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
