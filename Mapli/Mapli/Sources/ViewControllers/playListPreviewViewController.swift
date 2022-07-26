55ad8cad10a49f3a3034dd271e5d2489dd89be12
김상현 <kshy1019@naver.com>
2022-07-26 23:05:07 +0900

    [Feat] #6 collectionview 추가

--- Mapli/Mapli/Sources/ViewControllers/playListPreviewViewController.swift
+++ Mapli/Mapli/Sources/ViewControllers/playListPreviewViewController.swift
@@ -21,3 +21,41 @@ class playListPreviewViewController: UIViewController {
         templateImageView.image = UIImage(named: "blackBoard.png")
     }
 }
+
+extension playListPreviewViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TemplateCollectionViewCell", for: indexPath) as? TemplateCollectionViewCell else { return UICollectionViewCell() }
+        cell.setUI()
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.width * 0.8
+        let height = (collectionView.bounds.height * 0.1) - 4.5
+        let CGSize = CGSize(width: width, height: height)
+        return CGSize
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let inset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        return inset
+    }
+}
+
+class TemplateCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    func setUI() {
+        DispatchQueue.main.async {
+            self.titleLabel.text = "금요일에 만나요. - 아이유"
+        }
+    }
+}
