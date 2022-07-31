401622618ff997fd30068f0464319409ab9678d4
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-08-01 03:33:12 +0900

    [FEAT] AppleMusicAPI로 노래 목록 가져오기 구현 (#12)

---
+++ Mapli/Mapli/Sources/Classes/AppleMusicViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  AppleMusicViewModel.swift
+//  Mapli
+//
+//  Created by woo0 on 2022/08/01.
+//
+
+import Combine
+import Foundation
+import StoreKit
+
+final class AppleMusicViewModel: ObservableObject {
+	@Published var mySongs = [MySong]()
+	@Published var playlists = [Playlist]()
+	var usetToken = String()
+
+	init() {
+		SKCloudServiceController.requestAuthorization { (status) in
+			if status == .authorized {
+				self.fetchAPI()
+			}
+		}
+	}
+
+	func viewDidLoad() {
+		SKCloudServiceController.requestAuthorization { (status) in
+			if status == .authorized {
+				self.fetchAPI()
+			}
+		}
+	}
+}
+
+private extension AppleMusicViewModel {
+	func fetchAPI() {
+		Task {
+			do {
+				self.usetToken = try await AppleMusicAPI().fetchUserToken()
+				self.playlists = try await AppleMusicAPI().fetchPlaylists(userToken: usetToken)
+				self.mySongs = try await AppleMusicAPI().fetchSongs(userToken: usetToken, id: playlists[0].id)
+			} catch NetworkError.invalidURL {
+				print("Invalid URL ERROR!")
+			}
+		}
+	}
+}
