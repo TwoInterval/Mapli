ba6ec923f819f3e22c00faa116c824c2d1bccf2e
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-08-04 14:20:32 +0900

    Custom Cell Optional Casting

--- Mapli/Mapli/Sources/Cells/SongSelectionTableViewCell.swift
+++ Mapli/Mapli/Sources/Cells/SongSelectionTableViewCell.swift
@@ -13,7 +13,6 @@ class SongSelectionTableViewCell: UITableViewCell {
 	
     override func awakeFromNib() {
         super.awakeFromNib()
-        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
